360fd3d6d1258a3ddce90baab05ce48cff5992c2
Takuya Kitazawa <k.takuti@gmail.com>
2021-12-28 07:28:05 -0800

    Move content parse/filter logic to Post module

--- prelims/__init__.py
+++ prelims/__init__.py
@@ -9,40 +9,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 RE_FRONT_MATTER = re.compile(r'---\n([\s\S]*?\n)---\n')
 RE_PATH_TO_PERMALINK = re.compile(r'((/ja){0,1}/note/.+?)(\.md|\.html)')
-RE_FILTERS = [
-    # HTML tag
-    re.compile(r'<.*?>'),
-    # Markdown codefence / math block
-    re.compile(r'^(\$\$|```)(.*)\n(.*\n)+(\$\$|```)', re.MULTILINE),
-    # inline math
-    re.compile(r'\$(.*)\$'),
-    # URL
-    re.compile(r'https?:\/\/[\S]+')
-]
-
 
 def extract_contents(paths):
+    posts = []
     for path in paths:
         with open(path) as f:
-            content = f.read()
+            raw_content = f.read()
 
-        m = RE_FRONT_MATTER.search(content)
-
-        if m is None:
-            continue
-
-        # avoid recommending draft articles by making their contents empty
-        front_matter = yaml.safe_load(m.group(1))
-        if 'draft' in front_matter and front_matter['draft']:
-            continue
-
-        # remove front matter
-        content = content.replace(m.group(0), '')
-
-        for re_filter in RE_FILTERS:
-            content = re_filter.sub('', content)
-
-        yield Post(path, front_matter, content)
+        post = Post.create(path, raw_content)
+        if post.is_valid():
+            posts.append(post)
+    return posts
 
 
 def path_to_permalink(path):
@@ -101,7 +78,7 @@ def process(path_dir, tokenizer=None, custom_funcs=[]):
             f'path {path_dir} is not a directory or does not exist'
     paths = [os.path.join(path_dir, f) for f in os.listdir(path_dir)]
 
-    posts = list(extract_contents(paths))
+    posts = extract_contents(paths)
 
     recommend(posts, topk=3, tokenizer=tokenizer)
     for func in custom_funcs:
--- prelims/post.py
+++ prelims/post.py
@@ -1,11 +1,53 @@
+import re
+import yaml
+
+RE_FRONT_MATTER = re.compile(r'---\n([\s\S]*?\n)---\n')
+RE_FILTERS = [
+    # HTML tag
+    re.compile(r'<.*?>'),
+    # Markdown codefence / math block
+    re.compile(r'^(\$\$|```)(.*)\n(.*\n)+(\$\$|```)', re.MULTILINE),
+    # inline math
+    re.compile(r'\$(.*)\$'),
+    # URL
+    re.compile(r'https?:\/\/[\S]+')
+]
+
+
 class Post(object):
 
-    def __init__(self, path, front_matter, content):
+    def __init__(self, path, front_matter, raw_content, content):
         self.path = path
         self.front_matter = front_matter
+        self.raw_content = raw_content
         self.content = content
 
     def update(self, key, value, allow_overwrite=False):
         if key in self.front_matter and not allow_overwrite:
             return
         self.front_matter[key] = value
+
+    def is_draft(self):
+        # avoid processing draft articles
+        return 'draft' in self.front_matter and self.front_matter['draft']
+
+    def is_valid(self):
+        return self.front_matter is not None and len(self.content) > 0 and not self.is_draft()
+
+    @staticmethod
+    def create(path, raw_content):
+        front_matter = None
+        content = raw_content
+
+        m = RE_FRONT_MATTER.search(raw_content)
+
+        if m is not None:
+            front_matter = yaml.safe_load(m.group(1))
+
+            # remove front matter
+            content = raw_content.replace(m.group(0), '')
+
+        for re_filter in RE_FILTERS:
+            content = re_filter.sub('', content)
+
+        return Post(path, front_matter, raw_content, content)
