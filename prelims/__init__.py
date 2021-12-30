a52e1c41b568fca91b9291fa081826e59fe9a0c4
Takuya Kitazawa <k.takuti@gmail.com>
2021-12-29 21:20:08 -0800

    Implement a unified class for handling posts

--- prelims/__init__.py
+++ prelims/__init__.py
@@ -1,20 +1,14 @@
 from .processors.recommender import Recommender
-from .utils import load_posts, save_posts
-
-import os
+from .handler import PostsHandler
 
 
 def process(path_dir, permalink_base='', tokenizer=None, custom_processors=[]):
-    assert os.path.isdir(os.path.expanduser(path_dir)), \
-            f'path {path_dir} is not a directory or does not exist'
-    paths = [os.path.join(path_dir, f) for f in os.listdir(path_dir)]
 
-    posts = load_posts(paths)
-
+    handler = PostsHandler(path_dir)
     recommender = Recommender(permalink_base=permalink_base, topk=3,
                               tokenizer=tokenizer)
-    recommender.process(posts)
+    handler.register_processor(recommender)
     for processor in custom_processors:
-        processor.process(posts)
+        handler.register_processor(processor)
 
-    save_posts(posts)
+    handler.execute()
---
+++ prelims/handler.py
@@ -0,0 +1,35 @@
+from .post import Post
+
+import os
+
+
+class PostsHandler(object):
+
+    def __init__(self, path_dir):
+        assert os.path.isdir(os.path.expanduser(path_dir)), \
+            f'path {path_dir} is not a directory or does not exist'
+
+        self.paths = [os.path.join(path_dir, f) for f in os.listdir(path_dir)]
+        self.processors = []
+
+    def register_processor(self, processor):
+        self.processors.append(processor)
+
+    def execute(self):
+        posts = self.load_posts()
+        for processor in self.processors:
+            processor.process(posts)
+        self.save_posts(posts)
+
+    def load_posts(self):
+        posts = []
+        for path in self.paths:
+            post = Post.load(path)
+            if post.is_valid():
+                posts.append(post)
+        return posts
+
+    @staticmethod
+    def save_posts(posts):
+        for post in posts:
+            post.save()
