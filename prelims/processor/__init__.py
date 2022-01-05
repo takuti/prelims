f834ba70d8e45c4b1c4ed16c72bdd2673047973c
Takuya Kitazawa <k.takuti@gmail.com>
2022-01-04 21:28:48 -0800

    Add processor for extracting file paths
    Close #2

--- prelims/processor/__init__.py
+++ prelims/processor/__init__.py
@@ -1,5 +1,8 @@
 from .base import BaseFrontMatterProcessor
+from .og_file_path_extractor import OpenGraphFilePathExtractor
 from .recommender import Recommender
 
 
-__all__ = ['BaseFrontMatterProcessor', 'Recommender']
+__all__ = ['BaseFrontMatterProcessor',
+           'OpenGraphFilePathExtractor',
+           'Recommender']
---
+++ prelims/processor/tests/test_og_file_path_extractor.py
@@ -0,0 +1,42 @@
+from prelims import Post
+from prelims.processor import OpenGraphFilePathExtractor
+
+from unittest import TestCase
+
+
+class OpenGraphFilePathExtractorTestCase(TestCase):
+
+    def test_process(self):
+        extractor = OpenGraphFilePathExtractor(
+            image_base='/images', audio_base='/audio', video_base='/videos')
+
+        p = Post('/path/to/posts/a.md', {'title': 'foo'},
+                 '', """
+                 Image:
+
+                 ![img1](/images/foo/img1.png)
+
+                 Audio:
+
+                 <audio controls src="/audio/foo/music.mp3" type="audio/mpeg">
+                 </audio>
+
+                 Video:
+
+                 <video width="320" height="240" controls>
+                   <source src="/videos/foo/movie.mp4" type="video/mp4">
+                   Your browser does not support the video tag.
+                 </video>
+                 """)
+        extractor.process([p])
+
+        self.assertTrue('images' in p.front_matter)
+        self.assertTrue('audio' in p.front_matter)
+        self.assertTrue('videos' in p.front_matter)
+        self.assertEqual(sorted(p.front_matter.items()),
+                         sorted({
+                            'title': 'foo',
+                            'images': ['/images/foo/img1.png'],
+                            'audio': ['/audio/foo/music.mp3'],
+                            'videos': ['/videos/foo/movie.mp4']
+                         }.items()))
