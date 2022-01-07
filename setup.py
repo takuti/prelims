f6566590248b47abf84bcbf0173b96abc1bf57f9
Takuya Kitazawa <k.takuti@gmail.com>
2022-01-06 19:30:26 -0800

    Bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 DISTNAME = 'prelims'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'Front matter post-processor for static site generators'
 with open('README.md') as f:
     LONG_DESCRIPTION = f.read()
